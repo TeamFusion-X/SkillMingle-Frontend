75aee13f66fce6260aade0bccf54f893d2394602
codeforme1234 <deveshkumar_21294@aitpune.edu.in>
2024-03-10 23:46:51 +0530

    main page pe thoda sa kaam kya

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LoginSignupComponent } from './login-signup/login-signup.component';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [MainComponent, LoginSignupComponent, CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
